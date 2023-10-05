80526cdec58109a28cfdfe6600ebe0e7d6f21373
yuvalyanous1 <yuvaly612@gmail.com>
2023-10-05 14:37:42 +0300

    test-21

--- ui/play.py
+++ ui/play.py
@@ -6,10 +6,11 @@ def santiy(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://finviz.com/")
-    page.get_by_role("link", name="Screener").click()
+    page.wait_for_selector('link[name="Screener"]', timeout=5000)
     page.locator("#fs_cap").select_option("large")
+    page.wait_for_timeout(4000)
     page.get_by_role("link", name="AEP").click()
-
+    page.wait_for_timeout(4000)
     # ---------------------
     context.close()
     browser.close()
